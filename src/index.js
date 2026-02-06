ce0926f46e98f31d26b2e9f69b81efe03f3c0563
viewplay <guy.99110@gmail.com>
2026-02-06 07:09:01 +0100

    Allow PATCH in CORS

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
     res.setHeader("Vary", "Origin");
   }
 
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods","GET,POST,PATCH,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, X-Admin-Token");
   res.setHeader("Access-Control-Max-Age", "86400");
 
